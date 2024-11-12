adbbc5d745d206e3d86a8790c088486fa55038c1
tanay-44 <136118131+tanay-44@users.noreply.github.com>
2024-11-12 07:17:10 +0530

    Update check_planar.h

--- Project/check_planar.h
+++ Project/check_planar.h
@@ -7,16 +7,13 @@
 #include <boost/graph/boyer_myrvold_planar_test.hpp>
 #include <boost/graph/is_kuratowski_subgraph.hpp>
 #include <boost/property_map/property_map.hpp>
-
 #include <boost/graph/graphviz.hpp>
 #include <fstream>
 
 using namespace boost;
 using namespace std;
 
-typedef adjacency_list<vecS, vecS, undirectedS, 
-                       property<vertex_index_t, int>, 
-                       property<edge_index_t, int>> Graph;
+typedef adjacency_list<vecS, vecS, undirectedS,property<vertex_index_t, int>,property<edge_index_t, int>> Graph;
 typedef vector<graph_traits<Graph>::edge_descriptor> kuratowski_edges_t;
 
 // Function declarations
